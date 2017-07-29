db0dc4da129c4a93398a92faf7cba2a0682fed32
BubbleM <1870408473@qq.com>
2017-07-29 16:05:12 +0800

    add reduce half practice

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+  let result = 0;
+  collection.forEach(item => result += item)
+  return result / collection.length;
 }
 
 module.exports = compute_average;
-
