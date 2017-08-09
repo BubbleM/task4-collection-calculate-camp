075e904a2bf8eac1add1042839ec09ff81549b8b
BubbleM <1870408473@qq.com>
2017-08-09 21:26:55 +0800

    update single_elememt

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,11 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  let result = [];
+  for(let i = 1; i < collection.length; i++) {
+    if (i % 2 === 0){
+      result.push(collection[i]);
+    }
+  }
+  return result.includes(element);
 };
 module.exports = is_exist_element;
--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,21 @@
 'use strict';
 var single_element = function(collection){
-
+  let result = [];
+  let repet = [];
+  for (let i = 1; i < collection.length + 1; i++) {
+    if (repet.includes(collection[i])){
+      break;
+    }
+    if (i % 2 === 0) {
+      if (result.includes(collection[i - 1])){
+        repet.push(collection[i - 1]);
+        let index = result.indexOf(collection[i - 1]);
+        result.splice(index, 1); // 删除重复元素
+        continue;
+      }
+      result.push(collection[i-1]);
+    }
+  }
+  return result;
 };
 module.exports = single_element;
