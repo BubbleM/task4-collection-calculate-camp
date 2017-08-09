19539cab014b73b12cfe621fac608b8d75db7761
BubbleM <1870408473@qq.com>
2017-08-09 21:49:01 +0800

    update even_group_calculate

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,7 +1,33 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+function rank_by_two_large_one_small(collection) {
+  let littleIndex = 0;
+  let littleNumber = 0;
+  let littleElement = [];
+  let upperElement = [];
+  let result = [];
+
+  collection.sort((a, b) => a - b > 0 ? 1 : -1);
+
+  for (let index = 0; index < collection.length; ++index) {
+    if (index === littleIndex && littleElement.length < parseInt(collection.length / 3)) {
+      littleElement.push(collection[index]);
+      littleIndex += 3;
+    } else {
+      upperElement.push(collection[index]);
+    }
+
+  }
+
+  littleIndex = 2
+  for (let index = 0; index < upperElement.length; ++index) {
+    if (index == littleIndex) {
+      result.push(littleElement[littleNumber]);
+      littleIndex += 2;
+      ++littleNumber;
+    }
+    result.push(upperElement[index]);
+  }
+
+  return result;
 }
 module.exports = rank_by_two_large_one_small;
-var collection_a = [2, 8, 1, 9, 6, 4, 3, 10];
-// expect(result).toEqual([2, 3, 1, 6, 8, 4, 9, 10])
--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,69 @@
 'use strict';
 var even_group_calculate_average = function(collection){
-
+  let result = [];
+  let evenIndex = getEvenIndex(collection);
+  let even = getEvenArr(evenIndex);
+  let resultObj = spiltArrWithLength(even);
+  if (resultObj.oneLength.length !== 0) {
+    result.push(getAverage(resultObj.oneLength));
+  }
+  if (resultObj.twoLength.length !== 0) {
+    result.push(getAverage(resultObj.twoLength));
+  }
+  if (resultObj.threeLength.length !== 0) {
+    result.push(getAverage(resultObj.threeLength));
+  }
+  if (resultObj.oneLength.length === 0 && resultObj.twoLength.length === 0 && resultObj.threeLength.length === 0) {
+    result.push(0);
+  }
+  return result;
 };
+
+function getEvenIndex(collection) {
+  let even = [];
+  for (let i = 1; i < collection.length + 1; i++) {
+    if (i % 2 === 0) {
+      even.push(collection[i - 1]);
+    }
+  }
+  return even;
+}
+
+function getEvenArr(collection) {
+  let result = [];
+  collection.forEach(item => {
+    if (item % 2 === 0) {
+      result.push(item);
+    }
+  })
+  return result;
+}
+
+
+function spiltArrWithLength(collection) {
+  let obj = {
+    oneLength: [],
+    twoLength: [],
+    threeLength: []
+  };
+  collection.forEach(item => {
+    if (item > 0 && item < 10) {
+      obj.oneLength.push(item);
+    } else if (item < 100) {
+      obj.twoLength.push(item);
+    } else {
+      obj.threeLength.push(item);
+    }
+  });
+  return obj;
+}
+
+function getAverage(collection) {
+  let total = 0;
+  collection.forEach(item => {
+    total += item;
+  });
+  return total / collection.length;
+}
+
 module.exports = even_group_calculate_average;
