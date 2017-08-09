88c57ad4767708bafce27390a9bbdbf62a1fec26
BubbleM <1870408473@qq.com>
2017-08-09 21:33:49 +0800

    update calculate_median

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,22 @@
 'use strict';
 var calculate_median = function(collection){
-
+  let even = [];
+  for (let i = 1; i < collection.length + 1; i++) {
+    if (i % 2 === 0) {
+      even.push(collection[i - 1]);
+    }
+  }
+  return getMedian(even);
 };
+
+function getMedian(arr) {
+  let result;
+  if (arr.length % 2 === 0) {
+    result = (arr[parseInt(arr.length / 2)] + arr[parseInt(arr.length / 2) - 1]) / 2;
+  } else {
+    result = arr[parseInt(arr.length / 2)];
+  }
+  return result;
+}
+
 module.exports = calculate_median;
